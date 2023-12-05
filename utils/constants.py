fix scale factor of 100, but did not update thresholds

utils/constants.py
@@ -276,28 +276,34 @@ class VENTHISTOGRAMFields(object):
     NUMBINS = 50
     XTICKS = np.linspace(0, XLIM, 4)
     YTICKS = np.linspace(0, YLIM, 5)
+    XTICKLABELS = ["{:.2f}".format(x) for x in XTICKS]
+    YTICKLABELS = ["{:.2f}".format(x) for x in YTICKS]
 
 
 class RBCHISTOGRAMFields(object):
     """Ventilation histogram fields."""
 
     COLOR = (247.0 / 255, 96.0 / 255, 111.0 / 255)
-    XLIM = 1.2
+    XLIM = 0.012
     YLIM = 0.1
     NUMBINS = 50
     XTICKS = np.linspace(0, XLIM, 4)
     YTICKS = np.linspace(0, YLIM, 5)
+    XTICKLABELS = ["{:.1e}".format(x) for x in XTICKS]
+    YTICKLABELS = ["{:.2f}".format(x) for x in YTICKS]
 
 
 class MEMBRANEHISTOGRAMFields(object):
     """Membrane histogram fields."""
 
     COLOR = (0.4, 0.7608, 0.6471)
-    XLIM = 2.5
+    XLIM = 0.025
     YLIM = 0.18
     NUMBINS = 70
     XTICKS = np.linspace(0, XLIM, 4)
     YTICKS = np.linspace(0, YLIM, 5)
+    XTICKLABELS = ["{:.1e}".format(x) for x in XTICKS]
+    YTICKLABELS = ["{:.2f}".format(x) for x in YTICKS]
 
 
 class PDFOPTIONS(object):

utils/plot.py
@@ -4,6 +4,7 @@ import sys
 from typing import Dict, List, Optional, Tuple
 
 import skimage
+from matplotlib.ticker import ScalarFormatter
 
 sys.path.append("..")
 import cv2
@@ -371,6 +372,8 @@ def plot_histogram(
     refer_fit: Tuple[float, float, float],
     xticks: Optional[List[float]] = None,
     yticks: Optional[List[float]] = None,
+    xticklabels: Optional[List[str]] = None,
+    yticklabels: Optional[List[str]] = None,
 ):
     """Plot histogram of arbitrary data.
 
@@ -384,6 +387,8 @@ def plot_histogram(
         refer_fit (Tuple[float, float, float]): fit parameters of the healthy reference.
         xticks (Optional[List[float]], optional): x ticks. Defaults to None.
         yticks (Optional[List[float]], optional): y ticks. Defaults to None.
+        xticklabels (Optional[List[str]], optional): x tick labels. Defaults to None.
+        yticklabels (Optional[List[str]], optional): y tick labels. Defaults to None.
     """
     # make a thick frame
     plt.rc("axes", linewidth=4)
@@ -402,15 +407,10 @@ def plot_histogram(
     # define and plot healthy reference line
     normal = refer_fit[0] * np.exp(-(((bins - refer_fit[1]) / refer_fit[2]) ** 2))
     ax.plot(bins, normal, "--", color="k", linewidth=4)
-
-    # ax.set_xlabel('Pixel Intensity', fontsize=26)
-    ax.set_ylabel("Fraction of Total Pixels", fontsize=35)
     plt.xlim((0, xlim))
     plt.ylim((0, ylim))
     plt.locator_params(axis="x", nbins=4)
     try:
-        xticklabels = ["{:.1f}".format(x) for x in xticks]
-        yticklabels = ["{:.2f}".format(x) for x in yticks]
         plt.xticks(xticks, xticklabels, fontsize=35)
         plt.yticks(yticks, yticklabels, fontsize=35)
     except TypeError:

utils/report.py
@@ -59,15 +59,30 @@ def format_dict(dict_stats: Dict[str, Any]) -> Dict[str, Any]:
     ]
     # list of variables to round to 3 decimal places
     list_round_3 = [constants.StatsIOFields.RBC_M_RATIO]
+    # list of variables to output to scientific notation
+    list_scientific = [
+        constants.StatsIOFields.RBC_MEAN,
+        constants.StatsIOFields.MEMBRANE_MEAN,
+        constants.StatsIOFields.RBC_MEDIAN,
+        constants.StatsIOFields.MEMBRANE_MEDIAN,
+        constants.StatsIOFields.RBC_STDDEV,
+        constants.StatsIOFields.MEMBRANE_STDDEV,
+    ]
+
     for key in dict_stats.keys():
         if isinstance(dict_stats[key], float) and key in list_round_0:
             dict_stats[key] = int(np.round(dict_stats[key], 0))
         elif isinstance(dict_stats[key], float) and key in list_round_3:
             dict_stats[key] = np.round(dict_stats[key], 3)
+        elif isinstance(dict_stats[key], float) and key in list_scientific:
+            dict_stats[key] = "{:.1e}".format(dict_stats[key])
         elif isinstance(dict_stats[key], float) and (
-            key not in list_round_3 or key not in list_round_0
+            key not in list_round_3
+            or key not in list_round_0
+            or key not in list_scientific
         ):
             dict_stats[key] = np.round(dict_stats[key], 2)
+
     return dict_stats
 
 

utils/signal_utils.py
@@ -208,9 +208,7 @@ def calculate_flipangle_correction(fa_gas: float, fa_dis: float) -> float:
         fa_gas (float): gas flip angle in degrees
         fa_dis (float): dissolved flip angle in degrees
     """
-    return np.round(
-        (100 * np.sin(fa_gas * np.pi / 180) / np.sin(fa_dis * np.pi / 180)), 3
-    )
+    return np.round((np.sin(fa_gas * np.pi / 180) / np.sin(fa_dis * np.pi / 180)), 3)
 
 
 def calculate_flipangle_factor(fa_gas: float, fa_dis: float) -> float: